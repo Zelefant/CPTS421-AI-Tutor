86c92f61f7b84025a396ddd92ae92f7520f1a982
Zelefant <zelefantplaysmc@outlook.com>
2025-10-05 13:58:25 -0700

    Edits to web UI

--- llmsite/tutor/views.py
+++ llmsite/tutor/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
-from django.views.decorators.http import require_http_methods, csrf_exempt
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 import json
 from languagemodel import StartAIChat, Initialization, SendMessage
 
@@ -13,7 +14,7 @@ def _session_id(request):
     return request.session.session_key
 
 def chat_page(request):
-    return render(request, "tutor/chat_api.html")
+    return render(request, "chat_api.html")
 
 @csrf_exempt
 @require_http_methods(["POST"])
